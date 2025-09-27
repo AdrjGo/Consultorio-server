269c0fb13d83a74b406eb0b193135c45f7b14f5a
AdrjGo <gcadrianj@gmail.com>
2025-09-26 23:09:33 -0400

    fix: move responses to managements responses

---
+++ Application/Responses/Managements/RoleResponse.cs
@@ -0,0 +1,9 @@
+namespace Application.Responses
+{
+    public class RoleResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
